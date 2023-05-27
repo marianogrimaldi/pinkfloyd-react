7248b58b4a4484c46d6b40c24eaa70478a0c51a0
marianogrimaldi <marianogdanielgrimaldi@gmail.com>
2023-05-27 20:28:21 -0300

    ultimo cambio

--- src/Components/CartWidget/cartWidget.jsx
+++ src/Components/CartWidget/cartWidget.jsx
@@ -1,17 +1,15 @@
 import { Link } from 'react-router-dom'
-import {SiDiscogs}  from "react-icons/Si"
 import { useContext } from 'react'
 import { CartContext } from '../../Context/CartContex'
 import "./cartWidget.scss"
-
+import cd from "../../assets/cd.png"
 
 const CartWidget = () => {
     const {totalCantidad} = useContext(CartContext)
-
     return(
-
         <Link to="/cart" className="flexCd"> 
-            <SiDiscogs className="carrito"/>
+            <img src={cd} className="carrito" alt="" />
+           
             <span className="cantidad">{totalCantidad()}</span>
          </Link>
     )
